94e7ad299794352de59b891963c4800e27500a4b
Ashwin <ashwinm.045@gmail.com>
2024-07-13 22:37:18 +0530

    Added .env configurations

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,14 +1,23 @@
 package config
 
 import (
+	"os"
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 )
 
 var DB *gorm.DB
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:10As2000*@tcp(127.0.0.1)/bookstore?charset=utf8&parseTime=True&loc=Local")
+	godotenv.Load()
+	databaseURI := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
+	os.Getenv("DATABASE_USER"),
+	os.Getenv("DATABASE_PASSWORD"),
+	os.Getenv("DATABASE_URL"),
+	os.Getenv("DATABASE_NAME"))
+	d, err := gorm.Open("mysql", databaseURI)
 	if err != nil {
 		panic(err)
 	}
